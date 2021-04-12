3c01abe7c5f8266b574ecb03b4c781578f4d7b1a
Ismail Alabi <alability99@gmail.com>
2021-04-12 10:09:18 +0100

    changed women to female

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -19,13 +19,13 @@ const HomePage = () => (
             </div>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>WOMENS</h1>
+                    <h1 className='title'>FEMALE</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>MEN</h1>
+                    <h1 className='title'>MEN CORPORATE</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
