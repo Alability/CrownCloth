c7fb7386aa42cc000556dfcc65ee2916e789544f
Ismail Alabi <alability99@gmail.com>
2021-08-08 22:12:26 +0100

    collection routing and selector, and also cleared nodesass error

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,13 +10,14 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import logo from '../../assets/logo.png';
 
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
-            <Logo className='logo' />
+            <img src={logo} alt="CARE-PLUS LOGO"/>
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>
